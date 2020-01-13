5f0b4bf3b004df1fd307fa3e284ede031d31f8e3
Guillaume Villena <guiguivil@gmail.com>
2020-01-13 13:12:11 -0800

    Using CKEditor Full as editor

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import SimpleMDE from 'simplemde';
-import hljs from 'highlight.js';
+import CKEditor from 'ckeditor4-react';
 
-const classNames = [
-  'editor-toolbar',
-  'CodeMirror',
-  'editor-preview-side',
-  'editor-statusbar'
-];
 
 class MarkdownEditor extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   componentDidMount() {
     this.create();
-    window.hljs = hljs; // TODO: fix this after the next release of SimpleMDE
   }
 
   shouldComponentUpdate(nextProps) {
@@ -30,46 +26,20 @@ class MarkdownEditor extends Component {
   }
 
   create() {
-    const { onChange, onSave } = this.props;
-    let opts = Object.create(this.props);
-    opts['element'] = this.refs.text;
-    opts['autoDownloadFontAwesome'] = false;
-    opts['spellChecker'] = false;
-    opts['renderingConfig'] = {
-      codeSyntaxHighlighting: true
-    };
-    let toolbarIcons = [
-      'bold', 'italic', 'heading', '|',
-      'code', 'quote', 'unordered-list', 'ordered-list',
-      '|', 'link', 'image', '|', 'preview', 'side-by-side', 'fullscreen', '|'
-    ];
-    if (onSave) {
-      toolbarIcons.push({
-        name: 'save',
-        action: () => {
-          onSave();
-        },
-        className: 'fa fa-floppy-o',
-        title: 'Save'
-      });
-    }
-    opts['toolbar'] = toolbarIcons;
-    this.editor = new SimpleMDE(opts);
-    this.editor.codemirror.on('change', () => {
-      onChange(this.editor.value());
-    });
   }
 
   destroy() {
-    for (let i in classNames) {
-      let elementToRemove = this.refs.container.querySelector('.' + classNames[i]);
-      elementToRemove && elementToRemove.remove();
-    }
+
   }
 
   render() {
-    return React.createElement('div', { ref: 'container' },
-      React.createElement('textarea', { ref: 'text' })
+    return (
+      <div>
+        <CKEditor
+          data={this.props.initialValue}
+          onChange={evt => this.props.onChange(evt.editor.getData())}
+        />
+      </div>
     );
   }
 }
